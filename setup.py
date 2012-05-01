13c48703e4d1e52d10002c13140eb09bcec4fea0
Vincent Rabaud <vrabaud@willowgarage.com>
2012-05-01 15:23:10 -0700

    Imported Upstream version 0.2.3

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(name='object_recognition_tod',
+      version='1.0.0',
+      description='A pipeline for object recognition implementing a bag of feature approach',
+      packages=['object_recognition_tod'],
+      package_dir={'':'python'}
+)
