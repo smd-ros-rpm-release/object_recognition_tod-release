30267b6c768c6e577654cb0b861cadc06d1ac591
Vincent Rabaud <vrabaud@willowgarage.com>
2013-04-14 22:06:43 +0200

    Rebase from 'release/fuerte/object_recognition_tod'

--- python/object_recognition_tod/trainer.py
+++ python/object_recognition_tod/trainer.py
@@ -3,160 +3,53 @@
 Module defining the TOD trainer to train the TOD models
 """
 
+from ecto import BlackBoxCellInfo as CellInfo, BlackBoxForward as Forward
 from ecto_opencv import calib, features2d, highgui
 from ecto_opencv.features2d import FeatureDescriptor
-from object_recognition_core.pipelines.training import TrainingPipeline
-from object_recognition_core.utils.json_helper import dict_to_cpp_json_str
+from object_recognition_core.ecto_cells.db import ModelWriter
+from object_recognition_core.pipelines.training import TrainerBase
 from object_recognition_tod import ecto_training
 import ecto
+from ecto import BlackBoxCellInfo as CellInfo, BlackBoxForward as Forward
 
 ########################################################################################################################
-class TODModelBuilder(ecto.BlackBox):
-    """
-    """
-    feature_descriptor = FeatureDescriptor
-    def declare_params(self, p):
-        p.declare('visualize', 'If true, displays images at runtime', False)
-        p.forward('json_feature_params', cell_name='feature_descriptor', cell_key='json_feature_params')
-        p.forward('json_descriptor_params', cell_name='feature_descriptor', cell_key='json_descriptor_params')
 
-    def declare_io(self, p, i, o):
-        self.source = ecto.PassthroughN(items=dict(image='An image',
-                                                   depth='A depth image',
-                                                   mask='A mask for valid object pixels.',
-                                                   K='The camera matrix',
-                                                   R='The rotation matrix',
-                                                   T='The translation vector',
-                                                   frame_number='The frame number.'
-                                                   )
-                                        )
-        self.model_stacker = ecto_training.TodModelStacker()
-
-        i.forward_all('source')
-        o.forward_all('model_stacker')
-
-    def configure(self, p, i, o):
-        self.depth_to_3d_sparse = calib.DepthTo3dSparse()
-        self.keypoints_to_mat = features2d.KeypointsToMat()
-        self.camera_to_world = ecto_training.CameraToWorld()
-        self.model_stacker = ecto_training.TodModelStacker()
-        from ecto_image_pipeline.base import RescaledRegisteredDepth
-        self.rescale_depth = RescaledRegisteredDepth() #this is for SXGA mode scale handling.
-        self.keypoint_validator = ecto_training.KeypointsValidator()
-        self.visualize = p.visualize
-
-    def connections(self):
-        graph = []
-        # connect the input
-        if 'depth' in self.feature_descriptor.inputs.keys():
-            graph += [self.rescale_depth['depth'] >> self.feature_descriptor['depth']]
-        graph += [self.source['image'] >> self.feature_descriptor['image'],
-                           self.source['image'] >> self.rescale_depth['image'],
-                           self.source['mask'] >> self.feature_descriptor['mask'],
-                           self.source['depth'] >> self.rescale_depth['depth'],
-                           self.source['K'] >> self.depth_to_3d_sparse['K']]
-
-        # Make sure the keypoints are in the mask and with a valid depth
-        graph += [self.feature_descriptor['keypoints', 'descriptors'] >> 
-                            self.keypoint_validator['keypoints', 'descriptors'],
-                            self.source['K'] >> self.keypoint_validator['K'],
-                            self.source['mask'] >> self.keypoint_validator['mask'],
-                            self.rescale_depth['depth'] >> self.keypoint_validator['depth'] ]
-
-        # transform the keypoints/depth into 3d points
-        graph += [ self.keypoint_validator['points'] >> self.depth_to_3d_sparse['points'],
-                        self.rescale_depth['depth'] >> self.depth_to_3d_sparse['depth'],
-                        self.source['R'] >> self.camera_to_world['R'],
-                        self.source['T'] >> self.camera_to_world['T'],
-                        self.depth_to_3d_sparse['points3d'] >> self.camera_to_world['points']]
-
-        # store all the info
-        graph += [ self.camera_to_world['points'] >> self.model_stacker['points3d'],
-                        self.keypoint_validator['points', 'descriptors', 'disparities'] >>
-                                                            self.model_stacker['points', 'descriptors', 'disparities'],
-                        self.source['K', 'R', 'T'] >> self.model_stacker['K', 'R', 'T'],
-                        ]
-
-        if self.visualize:
-            mask_view = highgui.imshow(name="mask")
-            depth_view = highgui.imshow(name="depth")
-
-            graph += [ self.source['mask'] >> mask_view['image'],
-                       self.source['depth'] >> depth_view['image']]
-            # draw the keypoints
-            keypoints_view = highgui.imshow(name="Keypoints")
-            draw_keypoints = features2d.DrawKeypoints()
-            graph += [ self.source['image'] >> draw_keypoints['image'],
-                       self.feature_descriptor['keypoints'] >> draw_keypoints['keypoints'],
-                       draw_keypoints['image'] >> keypoints_view['image']]
-
-        return graph
-
-
-class TODPostProcessor(ecto.BlackBox):
-    """
-    """
-    point_merger = ecto_training.PointMerger
-    model_filler = ecto_training.ModelFiller
-
-    def declare_params(self, p):
-        pass
-
-    def declare_io(self, p, i, o):
-        self.source = ecto.PassthroughN(items=dict(K='The camera matrix',
-                                                   quaternions='A vector of quaternions',
-                                                   Ts='A vector of translation vectors',
-                                                   disparities='The disparities of the measurements',
-                                                   descriptors='A stacked vector of descriptors',
-                                                   points='The 2D measurements per point.',
-                                                   points3d='The estimated 3d position of the points (3-channel matrices).'
-                                                   )
-                                        )
-        i.forward_all('source')
-        o.forward_all('model_filler')
-
-    def configure(self, p, i, o):
-        self.point_merger = self.point_merger()
-        self.model_filler = self.model_filler()
-
-    def connections(self):
-        return [ self.source['descriptors'] >> self.point_merger['descriptors'],
-                 self.point_merger['points', 'descriptors'] >> self.model_filler['points', 'descriptors']
-                 ]
-
-class TODTrainingPipeline(TrainingPipeline):
-    '''Implements the training pipeline functions'''
+class TodTrainer(ecto.BlackBox, TrainerBase):
+    def __init__(self, *args, **kwargs):
+        ecto.BlackBox.__init__(self, *args, **kwargs)
+        TrainerBase.__init__(self)
 
     @classmethod
-    def type_name(cls):
-        return "TOD"
+    def declare_cells(cls, _p):
+        # passthrough cells
+        cells = {'object_id': CellInfo(ecto.Constant),
+                 'json_db': CellInfo(ecto.Constant)}
+
+        # 'real cells'
+        cells.update({'model_filler': ecto_training.ModelFiller(),
+                      'model_writer': CellInfo(ModelWriter, params={'method':'TOD'}),
+                      'trainer': CellInfo(ecto_training.Trainer)})
+
+        return cells
 
     @classmethod
-    def incremental_model_builder(cls, *args, **kwargs):
-        subtype = kwargs['subtype']
-        pipeline_params = kwargs['pipeline_params']
-        args = kwargs['args']
+    def declare_forwards(cls, _p):
+        p = {'json_db': [Forward('value', 'json_db')],
+             'object_id': [Forward('value', 'object_id')]}
+        p.update({'trainer': 'all'})
+        i = {}
+        o = {}
 
-        # get the feature parameters
-        if 'feature' not in pipeline_params:
-            raise RuntimeError("You must supply feature parameters for TOD.")
-        feature_params = pipeline_params.get("feature")
-        # get the descriptor parameters
-        if 'descriptor' not in pipeline_params:
-            raise RuntimeError("You must supply descriptor parameters for TOD.")
-        descriptor_params = pipeline_params.get('descriptor')
+        return (p,i,o)
 
-        #grab visualize if works.
-        visualize = getattr(args, 'visualize', False)
-        return TODModelBuilder(json_feature_params=dict_to_cpp_json_str(feature_params),
-                               json_descriptor_params=dict_to_cpp_json_str(descriptor_params),
-                               visualize=visualize)
+    def connections(self, p):
+        connections = [ self.object_id[:] >> self.trainer['object_id'],
+                        self.json_db[:] >> self.trainer['json_db'] ]
+        connections += [ self.trainer['descriptors', 'points'] >> self.model_filler['descriptors', 'points'] ]
 
-    @classmethod
-    def post_processor(cls, *args, **kwargs):
-        pipeline_params = kwargs['pipeline_params']
-        search_params = pipeline_params.get("search", False)
+        # Connect the model builder to the source
+        connections += [ self.object_id[:] >> self.model_writer['object_id'],
+                         self.json_db[:] >> self.model_writer['json_db'],
+                         self.model_filler['db_document'] >> self.model_writer['db_document']]
 
-        if not search_params:
-            raise RuntimeError("You must supply search parameters for TOD.")
-        return TODPostProcessor(json_search_params=dict_to_cpp_json_str(search_params))
+        return connections
